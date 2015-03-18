aef62696cc0fd00bb096eac214f4da99c2e02b40
Thomas COIN <esvcorp@gmail.com>
2015-03-18 20:35:57 +0100

    Adding in XBee class all getters, setters, signals for AT Command Addressing params

--- src/qtxb/xbee.cpp
+++ src/qtxb/xbee.cpp
@@ -125,6 +125,149 @@ void XBee::unicast(QByteArray address, QString data)
     send(request);
 }
 
+void XBee::loadAddressingProperties() {
+    ATCommand at;
+    at.setCommand(ATCommand::Command_DH); send(&at);
+    at.setCommand(ATCommand::Command_DL); send(&at);
+    at.setCommand(ATCommand::Command_MY); send(&at);
+    at.setCommand(ATCommand::Command_MP); send(&at);
+    at.setCommand(ATCommand::Command_NC); send(&at);
+    at.setCommand(ATCommand::Command_SH); send(&at);
+    at.setCommand(ATCommand::Command_SL); send(&at);
+    at.setCommand(ATCommand::Command_NI); send(&at);
+    at.setCommand(ATCommand::Command_SE); send(&at);
+    at.setCommand(ATCommand::Command_DE); send(&at);
+    at.setCommand(ATCommand::Command_CI); send(&at);
+    at.setCommand(ATCommand::Command_TO); send(&at);
+    at.setCommand(ATCommand::Command_NP); send(&at);
+    at.setCommand(ATCommand::Command_DD); send(&at);
+    at.setCommand(ATCommand::Command_CR); send(&at);
+}
+
+// Adressing
+bool XBee::setDH(const quint32 dh) {
+    ATCommand at;
+    at.setCommand(ATCommand::Command_DH);
+    at.setParameter(QByteArray::number(dh));
+    send(&at);
+    return true;
+}
+
+bool XBee::setDL(const quint32 dl) {
+    ATCommand at;
+    at.setCommand(ATCommand::Command_DL);
+    at.setParameter(QByteArray::number(dl));
+    send(&at);
+    return true;
+}
+
+bool XBee::setMY(const quint16 my) {
+    ATCommand at;
+    at.setCommand(ATCommand::Command_MY);
+    at.setParameter(QByteArray::number(my));
+    send(&at);
+    return true;
+}
+
+bool XBee::setMP(const quint16 mp) {
+    ATCommand at;
+    at.setCommand(ATCommand::Command_MP);
+    at.setParameter(QByteArray::number(mp));
+    send(&at);
+    return true;
+}
+
+bool XBee::setNC(const quint32 nc) {
+    ATCommand at;
+    at.setCommand(ATCommand::Command_NC);
+    at.setParameter(QByteArray::number(nc));
+    send(&at);
+    return true;
+}
+
+bool XBee::setSH(const quint32 sh) {
+    ATCommand at;
+    at.setCommand(ATCommand::Command_SH);
+    at.setParameter(QByteArray::number(sh));
+    send(&at);
+    return true;
+}
+
+bool XBee::setSL(const quint32 sl) {
+    ATCommand at;
+    at.setCommand(ATCommand::Command_SL);
+    at.setParameter(QByteArray::number(sl));
+    send(&at);
+    return true;
+}
+
+bool XBee::setNI(const QString & ni) {
+    ATCommand at;
+    at.setCommand(ATCommand::Command_NI);
+    at.setParameter(ni.toUtf8());
+    send(&at);
+    return true;
+}
+
+bool XBee::setSE(const quint8 se) {
+    ATCommand at;
+    at.setCommand(ATCommand::Command_SE);
+    at.setParameter(QByteArray::number(se));
+    send(&at);
+    return true;
+}
+
+bool XBee::setDE(const quint8 de) {
+    ATCommand at;
+    at.setCommand(ATCommand::Command_DE);
+    at.setParameter(QByteArray::number(de));
+    send(&at);
+    return true;
+}
+
+bool XBee::setCI(const quint8 ci) {
+    ATCommand at;
+    at.setCommand(ATCommand::Command_CI);
+    at.setParameter(QByteArray::number(ci));
+    send(&at);
+    return true;
+}
+
+bool XBee::setTO(const quint8 to) {
+    ATCommand at;
+    at.setCommand(ATCommand::Command_TO);
+    at.setParameter(QByteArray::number(to));
+    send(&at);
+    return true;
+}
+
+bool XBee::setNP(const quint8 np) {
+    ATCommand at;
+    at.setCommand(ATCommand::Command_NP);
+    at.setParameter(QByteArray::number(np));
+    send(&at);
+    return true;
+}
+
+bool XBee::setDD(const quint16 dd) {
+    ATCommand at;
+    at.setCommand(ATCommand::Command_DD);
+    at.setParameter(QByteArray::number(dd));
+    send(&at);
+    return true;
+}
+
+bool XBee::setCR(const quint8 cr) {
+    ATCommand at;
+    at.setCommand(ATCommand::Command_CR);
+    at.setParameter(QByteArray::number(cr));
+    send(&at);
+    return true;
+}
+
+//_________________________________________________________________________________________________
+//___________________________________________PRIVATE API___________________________________________
+//_________________________________________________________________________________________________
 void XBee::readData()
 {
     unsigned startDelimiter = 0x7E;
@@ -149,7 +292,6 @@ void XBee::readData()
 
 void XBee::processPacket(QByteArray packet)
 {
-
     unsigned packetType = (unsigned char)packet.at(3);
     qDebug() << Q_FUNC_INFO << "packet type :" << QString::number(packetType, 16).prepend("0x");
 
@@ -157,6 +299,7 @@ void XBee::processPacket(QByteArray packet)
     case DigiMeshPacket::ATCommandResponseFrame : {
         ATCommandResponse *response = new ATCommandResponse(packet);
         emit receivedATCommandResponse(response);
+        processATCommandRespone(response);
         response->deleteLater();
         break;
     }
@@ -199,12 +342,36 @@ void XBee::processPacket(QByteArray packet)
     default:
         qDebug() << "Error:  Unknown Packet: " << packet.toHex();
     }
-
 }
 
-//_________________________________________________________________________________________________
-//___________________________________________PRIVATE API___________________________________________
-//_________________________________________________________________________________________________
+void XBee::processATCommandRespone(const ATCommandResponse *rep) {
+    Q_ASSERT(rep);
+    ATCommand::ATCommandType at = rep->atCommand();
+    QByteArray data = rep->data();
+
+    qDebug() << Q_FUNC_INFO << "AT command" << ATCommand::atCommandToByteArray(at) << " : " << data.toHex();
+
+    switch(at) {
+    // Addressing
+    case ATCommand::Command_DH : m_dh = data.toInt(); emit DHChanged(m_dh); break;
+    case ATCommand::Command_DL : m_dl = data.toInt(); emit DLChanged(m_dl); break;
+    case ATCommand::Command_MY : m_my = data.toInt(); emit MYChanged(m_my); break;
+    case ATCommand::Command_MP : m_mp = data.toInt(); emit MPChanged(m_mp); break;
+    case ATCommand::Command_NC : m_nc = data.toInt(); emit NCChanged(m_nc); break;
+    case ATCommand::Command_SH : m_sh = data.toInt(); emit SHChanged(m_sh); break;
+    case ATCommand::Command_SL : m_sl = data.toInt(); emit SLChanged(m_sl); break;
+    case ATCommand::Command_NI : m_ni = data; emit NIChanged(m_ni); break;
+    case ATCommand::Command_SE : m_se = data.toInt(); emit SEChanged(m_se); break;
+    case ATCommand::Command_DE : m_de = data.toInt(); emit DEChanged(m_de); break;
+    case ATCommand::Command_CI : m_ci = data.toInt(); emit CIChanged(m_ci); break;
+    case ATCommand::Command_TO : m_to = data.toInt(); emit TOChanged(m_to); break;
+    case ATCommand::Command_NP : m_np = data.toInt(); emit NPChanged(m_np); break;
+    case ATCommand::Command_DD : m_dd = data.toInt(); emit DDChanged(m_dd); break;
+    case ATCommand::Command_CR : m_cr = data.toInt(); emit CRChanged(m_cr); break;
+    default:
+        qWarning() << Q_FUNC_INFO << "Unhandled AT command " << at;
+    }
+}
 
 bool XBee::initSerialConnection()
 {
--- src/qtxb/xbee.h
+++ src/qtxb/xbee.h
@@ -20,13 +20,62 @@ class XBee : public QObject
 public:
     explicit XBee(QObject *parent = 0);
     XBee(const QString & serialPort, QObject * parent = 0);
+    ~XBee();
 
-    bool setSerialPort(const QString & serialPort);
     bool applyDefaultSerialPortConfig();
-    void send(DigiMeshPacket *request);
     void broadcast(QString data);
     void unicast(QByteArray address, QString data);
-    ~XBee();
+    void send(DigiMeshPacket *request);
+
+    bool setSerialPort(const QString & serialPort);
+
+    // Adressing
+    bool setDH(const quint32 dh);
+    bool setDL(const quint32 dl);
+    bool setMY(const quint16 my);
+    bool setMP(const quint16 mp);
+    bool setNC(const quint32 nc);
+    bool setSH(const quint32 sh);
+    bool setSL(const quint32 sl);
+    bool setNI(const QString & ni);
+    bool setSE(const quint8 se);
+    bool setDE(const quint8 de);
+    bool setCI(const quint8 ci);
+    bool setTO(const quint8 to);
+    bool setNP(const quint8 np);
+    bool setDD(const quint16 dd);
+    bool setCR(const quint8 cr);
+    quint32 DH() const { return m_dh;}
+    quint32 DL() const { return m_dl;}
+    quint16 MY() const { return m_my;}
+    quint16 MP() const { return m_mp;}
+    quint32 NC() const { return m_nc;}
+    quint32 SH() const { return m_sh;}
+    quint32 SL() const { return m_sl;}
+    QString NI() const { return m_ni;}
+    quint8 SE() const { return m_se;}
+    quint8 DE() const { return m_de;}
+    quint8 CI() const { return m_ci;}
+    quint8 TO() const { return m_to;}
+    quint8 NO() const { return m_np;}
+    quint16 DD() const { return m_dd;}
+    quint8 CR() const { return m_cr;}
+signals:
+    void DHChanged(const quint32 dh);
+    void DLChanged(const quint32 dh);
+    void MYChanged(const quint16 my);
+    void MPChanged(const quint16 mp);
+    void NCChanged(const quint32 nc);
+    void SHChanged(const quint32 sh);
+    void SLChanged(const quint32 sl);
+    void NIChanged(const QString & ni);
+    void SEChanged(const quint8 se);
+    void DEChanged(const quint8 de);
+    void CIChanged(const quint8 ci);
+    void TOChanged(const quint8 to);
+    void NPChanged(const quint8 np);
+    void DDChanged(const quint16 dd);
+    void CRChanged(const quint8 cr);
 
 signals:
     void receivedATCommandResponse(ATCommandResponse *response);
@@ -38,6 +87,7 @@ signals:
     void receivedRemoteCommandResponse(RemoteCommandResponse *response);
 
 public slots:
+    void loadAddressingProperties();
     void displayATCommandResponse(ATCommandResponse *digiMeshPacket);
     void displayModemStatus(ModemStatus *digiMeshPacket);
     void displayTransmitStatus(TransmitStatus *digiMeshPacket);
@@ -52,11 +102,29 @@ private slots:
 private:
     bool initSerialConnection();
     void processPacket(QByteArray packet);
+    void processATCommandRespone(const ATCommandResponse * rep);
 
 private:
     QSerialPort *serial;
     bool xbeeFound;
     QByteArray buffer;
+
+    // Adressing
+    quint32 m_dh;
+    quint32 m_dl;
+    quint16 m_my;
+    quint16 m_mp;
+    quint32 m_nc;
+    quint32 m_sh;
+    quint32 m_sl;
+    QString m_ni;
+    quint8 m_se;
+    quint8 m_de;
+    quint8 m_ci;
+    quint8 m_to;
+    quint8 m_np;
+    quint16 m_dd;
+    quint8 m_cr;
 };
 
 #endif

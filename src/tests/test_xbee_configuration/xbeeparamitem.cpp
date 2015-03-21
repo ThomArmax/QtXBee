85878a0dfa584af010d3bf4111a0847297e0b9c7
Thomas COIN <esvcorp@gmail.com>
2015-03-21 08:44:48 +0100

    Added ATCommandRole in XBeeParamItem class

--- src/tests/test_xbee_configuration/xbeeparamitem.cpp
+++ src/tests/test_xbee_configuration/xbeeparamitem.cpp
@@ -1,14 +1,18 @@
 #include "xbeeparamitem.h"
 
 XBeeParamItem::XBeeParamItem(QObject *parent) :
-    Armax::Datamodels::AbstractListItem(parent)
+    Armax::Datamodels::AbstractListItem(parent),
+    m_name(QString()),
+    m_value(QVariant()),
+    m_at(ATCommand::Command_Undefined)
 {
 }
 
 XBeeParamItem::XBeeParamItem(const QString &name, const QVariant &value, QObject *parent) :
     Armax::Datamodels::AbstractListItem(parent),
     m_name(name),
-    m_value(value)
+    m_value(value),
+    m_at(ATCommand::Command_Undefined)
 {
 }
 
@@ -16,6 +20,7 @@ QHash<int,QByteArray> XBeeParamItem::roleNames() const {
     QHash<int, QByteArray> roles;
     roles[NameRole] = "name";
     roles[ValueRole] = "value";
+    roles[AtCommandRole] = "at";
     return roles;
 }
 
@@ -24,6 +29,7 @@ bool XBeeParamItem::setData(const QVariant &value, int role)
     switch(role) {
     case NameRole: return setName(value.toString());
     case ValueRole: return setValue(value);
+    case AtCommandRole: return setAtCommand((ATCommand::ATCommandType)value.toInt());
     }
     return false;
 }
@@ -33,6 +39,7 @@ QVariant XBeeParamItem::data(int role) const
     switch(role) {
     case NameRole: return name();
     case ValueRole: return value();
+    case AtCommandRole: return atCommand();
     }
     return QVariant();
 }
@@ -53,6 +60,16 @@ bool XBeeParamItem::setValue(const QVariant &value) {
         m_value = value;
         emit valueChanged(m_value);
         emit dataChanged(ValueRole);
+        return true;
+    }
+    return false;
+}
+
+bool XBeeParamItem::setAtCommand(const ATCommand::ATCommandType at) {
+    if(m_at != at) {
+        m_at = at;
+        emit valueChanged(m_at);
+        emit dataChanged(AtCommandRole);
         return true;
     }
     return false;
--- src/tests/test_xbee_configuration/xbeeparamitem.h
+++ src/tests/test_xbee_configuration/xbeeparamitem.h
@@ -4,13 +4,16 @@
 #include <QObject>
 #include <QtArmax-1.0/datamodels/listmodel.h>
 
+#include "atcommand.h"
+
 class XBeeParamItem : public Armax::Datamodels::AbstractListItem
 {
     Q_OBJECT
 public:
     enum Roles {
         NameRole = /*Qt::UserRole+1*/0,
-        ValueRole
+        ValueRole,
+        AtCommandRole
     };
 
     explicit XBeeParamItem(QObject *parent = 0);
@@ -18,9 +21,11 @@ public:
 
     QString name() const { return m_name; }
     QVariant value() const { return m_value; }
+    ATCommand::ATCommandType atCommand() const { return m_at; }
 
     bool setName(const QString & name);
     bool setValue(const QVariant & value);
+    bool setAtCommand(const ATCommand::ATCommandType at);
 
     virtual QHash<int,QByteArray> roleNames() const;
     virtual bool setData(const QVariant &value, int role);
@@ -35,6 +40,7 @@ public slots:
 protected:
     QString m_name;
     QVariant m_value;
+    ATCommand::ATCommandType m_at;
 };
 
 #endif // XBEEPARAMITEM_H

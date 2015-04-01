54de1f13234fe0dab89f4ce320503b72b477fd21
Thomas COIN <esvcorp@gmail.com>
2015-04-01 19:59:44 +0200

    Start to implement NodeIdentificationIndicatorFrame class

--- src/qtxb/nodeidentificationindicatorframe.cpp
+++ src/qtxb/nodeidentificationindicatorframe.cpp
@@ -1,10 +1,184 @@
 #include "nodeidentificationindicatorframe.h"
 
 NodeIdentificationIndicatorFrame::NodeIdentificationIndicatorFrame(QObject *parent) :
-    DigiMeshFrame(parent)
+    DigiMeshFrameResponse(parent),
+    m_senderAddr64          (0),
+    m_senderAddr16          (0),
+    m_receiveOptions        (0),
+    m_remoteAddr16          (0),
+    m_remoteAddr64          (0),
+    m_nodeIdentifier        (QString()),
+    m_remoteParent16        (0),
+    m_deviceType            (0),
+    m_sourceEvent           (0),
+    m_digiProfileId         (0),
+    m_digiManufacturerId    (0)
 {
     setFrameType(DigiMeshFrame::NodeIdentificationIndicatorFrame);
 }
-void NodeIdentificationIndicatorFrame::readPacket(QByteArray rx){
-    Q_UNUSED(rx)
+
+bool NodeIdentificationIndicatorFrame::setPacket(const QByteArray &packet)
+Q_DECL_OVERRIDE
+{
+    qDebug() << Q_FUNC_INFO << QString(packet.toHex());
+    qDebug() << Q_FUNC_INFO << "packet size" << packet.size();
+    bool bRet  = false;
+    int count = 0;
+    int pow = 0;
+    int packetSize = packet.size();
+    int niOffset =  0;
+    // Reset all data
+    m_packet.clear();
+    m_senderAddr64          = 0;
+    m_senderAddr16          = 0;
+    m_receiveOptions        = 0;
+    m_remoteAddr16          = 0;
+    m_remoteAddr64          = 0;
+    m_nodeIdentifier        = "";
+    m_remoteParent16        = 0;
+    m_deviceType            = 0;
+    m_sourceEvent           = 0;
+    m_digiProfileId         = 0;
+    m_digiManufacturerId    = 0;
+
+    m_packet = packet;
+
+    if(packetSize < 33) {
+        qWarning() << Q_FUNC_INFO << "Warning : bad packet size (got"
+                   << QString("got %1, expecter %2)").arg(packet.size()).arg(33);
+        goto exit;
+    }
+
+    qDebug() << Q_FUNC_INFO << "start delimiter" << QString("0x%1").arg(QString::number(packet.at(0), 16));
+    setStartDelimiter(packet.at(0));
+    setLength(packet.at(2) + (packet.at(1)<<8));
+    if(packetSize == packet.at(2)+4) {
+        int ft = packet.at(3)&0xFF;
+        // Check the frame type
+        if(ft != m_frameType) {
+            qWarning() << Q_FUNC_INFO << "Warning : bad frame type"
+                       << qPrintable(QString("(got 0x%1, 0x%2 expected)").arg(ft,0, 16).arg(m_frameType, 0, 16));
+            //goto exit;
+        }
+        count = 4;
+
+        // Get specific raw data
+        while(count < packetSize-1) {
+            m_data.append(packet.at(count));
+            count++;
+        }
+
+        // Get Sender 64 Bits Address
+        count = 11;
+        pow = 0;
+        while(count > 3) {
+            m_senderAddr64 += QString(packet.at(count)).toInt() << pow*8;
+            count--;
+            pow++;
+        }
+
+        // Get Sender 16 Bits Address
+        count = 13;
+        pow = 0;
+        while(count > 11) {
+            m_senderAddr16 += QString(packet.at(count)).toInt()<< pow*8;
+            count--;
+            pow++;
+        }
+        m_receiveOptions = packet.at(14);
+
+        // Get Remote 16 Bits Address
+        count = 16;
+        pow = 0;
+        while(count > 14) {
+            m_senderAddr16 += QString(packet.at(count)).toInt() << pow*8;
+            count--;
+            pow++;
+        }
+
+        // Get Remote 64 Bits Address
+        count = 24;
+        pow = 0;
+        while(count > 16) {
+            m_senderAddr64 += QString(packet.at(count)).toInt() << pow*8;
+            count--;
+            pow++;
+        }
+
+        // Get Node Identifier
+        count = 25;
+        qDebug() << Q_FUNC_INFO << "packet[25]=" << packet.at(count);
+        while(packet.at(count) != 0x00) {
+            qDebug() << Q_FUNC_INFO << packet.at(count);
+            m_nodeIdentifier.append(packet.at(count));
+
+            count ++;
+        }
+        niOffset = 25 + m_nodeIdentifier.size();
+
+        // Get Remote Parent 16 Bits Address
+        count = niOffset+2;
+        pow = 0;
+        while(count > niOffset) {
+            m_remoteParent16 += QString(packet.at(count)).toInt() << pow*8;
+            count--;
+            pow++;
+        }
+
+        // Get Device Type
+        m_deviceType = packet.at(niOffset+3);
+
+        // Get Source Event
+        m_sourceEvent = packet.at(niOffset+4);
+
+        // Get Digi Profile ID
+        count = niOffset+6;
+        pow = 0;
+        while(count > niOffset+4) {
+            m_digiProfileId += QString(packet.at(count)).toInt() << pow*8;
+            count--;
+            pow++;
+        }
+
+        // Get Digi Manufacturer ID
+        count = niOffset + 8;
+        pow = 0;
+        while(count > niOffset+6) {
+            m_digiManufacturerId += QString(packet.at(count)).toInt() << pow*8;
+            count--;
+            pow++;
+        }
+
+        // Get Checksum
+        setChecksum(packet.at(packetSize-1));
+    }
+
+exit:
+    return bRet;
+}
+
+QString NodeIdentificationIndicatorFrame::toString() {
+    QString str;
+    str.append(QString("Raw packet                   : 0x%1\n").arg(QString(packet().toHex())));
+    str.append(QString("Start delimiter              : 0x%1\n").arg(QString::number(startDelimiter(), 16)));
+    str.append(QString("Frame type                   : %1 (0x%2)\n").arg(frameTypeToString(frameType())).arg(QString::number(frameType(), 16)));
+    str.append(QString("Length                       : %1 bytes\n").arg(m_length));
+    if(!m_data.isEmpty())
+    str.append(QString("Data                         : 0x%1\n").arg(QString(m_data.toHex())));
+    else
+    str.append(QString("Data                         : No data\n"));
+    str.append(QString("Sender Address 64bits        : 0x%1\n").arg(m_senderAddr64, 0, 16));
+    str.append(QString("Sender Address 16bits        : 0x%1\n").arg(m_senderAddr16, 0, 16));
+    str.append(QString("Receive Options              : 0x%1\n").arg(m_receiveOptions, 0, 16));
+    str.append(QString("Remote Address 16bits        : 0x%1\n").arg(m_remoteAddr16, 0, 16));
+    str.append(QString("Remote Address 64bits        : 0x%1\n").arg(m_remoteAddr64, 0, 16));
+    str.append(QString("Node Identifier              : %1\n").arg(m_nodeIdentifier));
+    str.append(QString("Remote Parent Address 16bits : 0x%1\n").arg(m_remoteParent16, 0, 16));
+    str.append(QString("Device Type                  : 0x%1\n").arg(m_deviceType, 0, 16));
+    str.append(QString("Source Event                 : 0x%1\n").arg(m_sourceEvent, 0, 16));
+    str.append(QString("Digi Profile ID              : 0x%1\n").arg(m_digiProfileId, 0, 16));
+    str.append(QString("Digi Manufacturer ID         : 0x%1\n").arg(m_digiManufacturerId, 0, 16));
+    str.append(QString("Checksum                     : %1\n").arg(checksum()));
+
+    return str;
 }
--- src/qtxb/nodeidentificationindicatorframe.h
+++ src/qtxb/nodeidentificationindicatorframe.h
@@ -1,7 +1,7 @@
 #ifndef NODEIDENTIFICATIONINDICATORFRAME_H
 #define NODEIDENTIFICATIONINDICATORFRAME_H
-#include "digimeshframe.h"
-#include <QByteArray>
+
+#include "digimeshframeresponse.h"
 
 /**
  * @brief The NodeIdentificationIndicator frame is received when a module
@@ -9,12 +9,27 @@
  *
  * The data portion of this frame is similar to a network discovery response frame (see ND command).
  */
-class NodeIdentificationIndicatorFrame : public DigiMeshFrame
+class NodeIdentificationIndicatorFrame : public DigiMeshFrameResponse
 {
     Q_OBJECT
 public:
     explicit NodeIdentificationIndicatorFrame(QObject *parent = 0);
-    void readPacket(QByteArray rx);
+    virtual bool setPacket(const QByteArray &packet);
+
+    virtual QString toString();
+
+private:
+    quint64 m_senderAddr64;
+    quint16 m_senderAddr16;
+    quint8 m_receiveOptions;
+    quint16 m_remoteAddr16;
+    quint64 m_remoteAddr64;
+    QString m_nodeIdentifier;
+    quint16 m_remoteParent16;
+    quint8 m_deviceType;
+    quint8 m_sourceEvent;
+    quint16 m_digiProfileId;
+    quint16 m_digiManufacturerId;
 };
 
 #endif // NODEIDENTIFICATIONINDICATORFRAME_H
